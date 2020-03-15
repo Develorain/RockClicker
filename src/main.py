b1e3bd481d6f7e5669792017946cf15e19a66395
Ahmad <develorain@gmail.com>
2020-03-15 18:08:31 -0400

    Removed unnecessary code

--- src/main.py
+++ src/main.py
@@ -12,35 +12,29 @@ from shopscreen import ShopScreen
 def main():
     global running, display, rock
 
+    # Initialize pygame
     pygame.init()
     display = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
-    pygame.display.set_caption("RockClicker")
-    running = True
+    pygame.display.set_caption("Rock Clicker")
 
+    running = True
     state = State.MAIN_SCREEN
 
+    # Create screens
     mainScreen = MainScreen(display)
     gameScreen = GameScreen(display)
     shopScreen = ShopScreen(display)
 
+    # Game loop
     while running:
         for event in pygame.event.get():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if state == State.MAIN_SCREEN:
                     state = mainScreen.checkForClicks(state)
                 elif state == State.GAME_SCREEN:
-                    #rock.isBeingClicked()
-                    #state = shopButton.isBeingClicked(state)
-
                     state = gameScreen.checkForClicks(state)
                 elif state == State.SHOP_SCREEN:
                     state = shopScreen.checkForClicks(state)
-
-                    #upgradeButton1.isBeingClicked(state)
-                    #upgradeButton2.isBeingClicked(state)
-                    #upgradeButton3.isBeingClicked(state)
-                    #upgradeButton4.isBeingClicked(state)
-
 
             if event.type == pygame.QUIT:
                 running = False
@@ -50,30 +44,12 @@ def main():
         
         if state == State.MAIN_SCREEN:
             mainScreen.draw()
-            #drawMainScreen(startButton)
         elif state == State.GAME_SCREEN:
             gameScreen.draw()
-            #drawGameScreen(rock, gemText, shopButton)
         elif state == State.SHOP_SCREEN:
             shopScreen.draw()
-            #drawShopScreen(upgradeButton1, upgradeButton2, upgradeButton3, upgradeButton4)
 
         pygame.display.update()
-
-def drawMainScreen(startButton):
-    startButton.draw(display)
-
-def drawGameScreen(rock, gemText, shopButton):
-    display.fill(constants.BLACK)
-    rock.draw(display)
-    gemText.drawDynamic(display, "Gems: " + rock.getGemCountAsString())
-    shopButton.draw(display)
-
-def drawShopScreen(upgradeButton1, upgradeButton2, upgradeButton3, upgradeButton4):
-    upgradeButton1.draw(display)
-    upgradeButton2.draw(display)
-    upgradeButton3.draw(display)
-    upgradeButton4.draw(display)
 
 if __name__ == '__main__':
     main()
