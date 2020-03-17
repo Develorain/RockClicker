0436921751b1160d432bb500220e5394bff3b9e6
Ahmad <develorain@gmail.com>
2020-03-16 21:41:41 -0400

    Added costs to upgrades, and track of how many upgrades purchased

--- src/gemsandupgrades.py
+++ src/gemsandupgrades.py
@@ -1,44 +1,71 @@
 class GemsAndUpgrades():
+    # UPGRADE 1
     INCREMENT_AMOUNT = 1
-    PASSIVE_INCREMENT_AMOUNT = 1
-    PASSIVE_INCREMENT_TIMER = 1000  # 1 second per passive tick
+    UPGRADE_INCREMENT_COST = 100
+    INCREMENT_COUNT = 0
+
+    # UPGRADE 2
+    PASSIVE_AMOUNT = 1
+    UPGRADE_PASSIVE_COST = 50
+    PASSIVE_COUNT = 0
+    PASSIVE_TIMER = 1000  # 1 second per passive tick
     
     def __init__(self):
-        self.gemCount = 0
-        self.passiveIncrementTimer = 0
+        self.gemCount = 100000
+        self.passiveTimer = 0
     
     def handlePassive(self, deltaTime):
         self.addToPassiveTimer(deltaTime)
 
         if (self.isPassiveReady()):
-            self.resetPassiveIncrementTimer()
+            self.resetPassiveTimer()
             self.passiveIncrementGems()
 
     def incrementGems(self):
         self.gemCount = self.gemCount + self.INCREMENT_AMOUNT
     
     def passiveIncrementGems(self):
-        self.gemCount = self.gemCount + self.PASSIVE_INCREMENT_AMOUNT
+        self.gemCount = self.gemCount + self.PASSIVE_AMOUNT
     
     def upgradeIncrementAmount(self):
-        self.INCREMENT_AMOUNT = self.INCREMENT_AMOUNT + 5
-        print("Upgraded to " + str(self.INCREMENT_AMOUNT) + " per click")
+        if self.UPGRADE_INCREMENT_COST <= self.gemCount:
+            self.INCREMENT_AMOUNT = self.INCREMENT_AMOUNT + 5
+            self.gemCount = self.gemCount - self.UPGRADE_INCREMENT_COST
+            self.INCREMENT_COUNT = self.INCREMENT_COUNT + 1
+
+            print("New gem count:" + str(self.gemCount))
+            print("Upgraded to " + str(self.INCREMENT_AMOUNT) + " per click")
+        else:
+            print("Can't afford upgrade")
     
     def upgradePassiveIncrementAmount(self):
-        self.PASSIVE_INCREMENT_AMOUNT = self.PASSIVE_INCREMENT_AMOUNT + 1
-        print("Upgraded to " + str(self.PASSIVE_INCREMENT_AMOUNT) + " per second")
+        if self.UPGRADE_PASSIVE_COST <= self.gemCount:
+            self.PASSIVE_AMOUNT = self.PASSIVE_AMOUNT + 1
+            self.gemCount = self.gemCount - self.UPGRADE_PASSIVE_COST
+            self.PASSIVE_COUNT = self.PASSIVE_COUNT + 1
+
+            print("New gem count:" + str(self.gemCount))
+            print("Upgraded to " + str(self.PASSIVE_AMOUNT) + " per second")
+        else:
+            print("Can't afford upgrade")
     
     def getGemCount(self):
         return self.gemCount
+    
+    def getIncrementCount(self):
+        return self.INCREMENT_COUNT
+
+    def getPassiveCount(self):
+        return self.PASSIVE_COUNT
 
     def isPassiveReady(self):
-        if self.passiveIncrementTimer >= self.PASSIVE_INCREMENT_TIMER:
+        if self.passiveTimer >= self.PASSIVE_TIMER:
             return True
         
         return False
     
     def addToPassiveTimer(self, deltaTime):
-        self.passiveIncrementTimer += deltaTime
+        self.passiveTimer += deltaTime
     
-    def resetPassiveIncrementTimer(self):
-        self.passiveIncrementTimer = 0
+    def resetPassiveTimer(self):
+        self.passiveTimer = 0
--- src/shopscreen.py
+++ src/shopscreen.py
@@ -16,11 +16,19 @@ class ShopScreen(Screen):
     
     def init(self):
         # Create the components of the main screen
-        self.upgradeButton1 = Button("Upgrade 1", 350, 150, 100, 50)
-        self.upgradeButton2 = Button("Upgrade 2", 350, 225, 100, 50)
-        self.upgradeButton3 = Button("Upgrade 3", 350, 300, 100, 50)
-        self.upgradeButton4 = Button("Upgrade 4", 350, 375, 100, 50)
+        self.upgradeButton1 = Button("Upgrade Increment Amount: Cost 100", 150, 150, 500, 50)
+        self.upgradeButton2 = Button("Upgrade Passive Increment Amount: Cost 50", 150, 225, 500, 50)
+        self.upgradeButton3 = Button("Upgrade 3", 150, 300, 500, 50)
+        self.upgradeButton4 = Button("Upgrade 4", 150, 375, 500, 50)
         self.backButton = Button("Back", 700, 550, 100, 50)
+
+        # Labels
+        self.upgrade1Label = Text("Increment Amount:", 10, 450, constants.WHITE, "Arial", 30)
+        self.upgrade1Text = Text(str(self.gemsAndUpgrades.getIncrementCount()), 265, 450, constants.WHITE, "Arial", 30)
+
+        self.upgrade2Label = Text("Passive Increment Amount:", 10, 500, constants.WHITE, "Arial", 30)
+        self.upgrade2Text = Text(str(self.gemsAndUpgrades.getPassiveCount()), 375, 500, constants.WHITE, "Arial", 30)
+
         self.gemLabel = Text("Gem:", 10, 550, constants.WHITE, "Arial", 30)
         self.gemText = Text(str(self.gemsAndUpgrades.getGemCount()), 90, 550, constants.WHITE, "Arial", 30)
 
@@ -30,6 +38,12 @@ class ShopScreen(Screen):
         self.components.append(self.upgradeButton3)
         self.components.append(self.upgradeButton4)
         self.components.append(self.backButton)
+
+
+        self.components.append(self.upgrade1Label)
+        self.components.append(self.upgrade1Text)
+        self.components.append(self.upgrade2Label)
+        self.components.append(self.upgrade2Text)
         self.components.append(self.gemLabel)
         self.components.append(self.gemText)
 
@@ -37,6 +51,8 @@ class ShopScreen(Screen):
         self.gemsAndUpgrades.handlePassive(deltaTime)
 
         self.gemText.update(str(self.gemsAndUpgrades.getGemCount()))
+        self.upgrade1Text.update(str(self.gemsAndUpgrades.getIncrementCount()))
+        self.upgrade2Text.update(str(self.gemsAndUpgrades.getPassiveCount()))
     
     def checkForComponentClicks(self, state):
         if self.upgradeButton1.isBeingClicked(state) == True:
